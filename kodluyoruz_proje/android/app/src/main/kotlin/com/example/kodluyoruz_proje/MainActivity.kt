6e4ad8fbdb229735155d4b727fbd9b999d645f80
ayelif <21060639@stu.omu.edu.tr>
2025-01-25 18:07:51 +0300

    proje

---
+++ kodluyoruz_proje/android/app/src/main/kotlin/com/example/kodluyoruz_proje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kodluyoruz_proje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ödev2/odev2.dart
@@ -0,0 +1,6 @@
+double alanHesapla ( double uzunKenar, double kisaKenar){
+sonuc= uzunKenar*kısaKenar;
+
+return sonuc; 
+}
+
